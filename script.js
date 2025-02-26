04e7cbf03ce4923f9dad21f942938342904e280d
rudra-in <rudra.org.in@gmail.com>
2025-02-26 21:26:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactivity or dynamic content here
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
